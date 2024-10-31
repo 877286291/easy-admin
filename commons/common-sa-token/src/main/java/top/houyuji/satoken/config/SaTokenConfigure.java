217ff9a290bf87d3b24249f9c7402b78d7377da5
houyj-dn-sy <yuji.hou@unitalen.com>
2024-10-31 14:22:52 +0800

    fix: sa-token鉴权失效

--- commons/common-sa-token/src/main/java/top/houyuji/satoken/config/SaTokenConfigure.java
+++ commons/common-sa-token/src/main/java/top/houyuji/satoken/config/SaTokenConfigure.java
@@ -20,38 +20,39 @@ public class SaTokenConfigure implements WebMvcConfigurer {
     // 注册拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        String apiVersion = "/api/v1";
         // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
         registry.addInterceptor(new SaInterceptor(handle -> {
-            SaRouter.match("/**", apiVersion + "/auth/login", r -> {
-                StpUtil.checkLogin();
-                UserInfoDTO currentUser = SaTokenUtil.getCurrentUser();
-                UserContext.set(new UserInfo(currentUser.getId(), currentUser.getUsername(), currentUser.getSysCode()));
-            });
-            SaRouter.match(apiVersion + "/sys/**", r -> StpUtil.checkRole("admin"));
+            SaRouter.match("/**")
+                    .notMatch("/auth/login")
+                    .check(r -> {
+                        StpUtil.checkLogin();
+                        UserInfoDTO currentUser = SaTokenUtil.getCurrentUser();
+                        UserContext.set(new UserInfo(currentUser.getId(), currentUser.getUsername(), currentUser.getSysCode()));
+                    });
+            SaRouter.match("/sys/**", r -> StpUtil.checkRole("admin"));
             // 用户管理
-            SaRouter.match(SaHttpMethod.POST).match(apiVersion + "/sys/user", r -> StpUtil.checkPermission("sys:user:add"));
-            SaRouter.match(SaHttpMethod.DELETE).match(apiVersion + "/sys/user", r -> StpUtil.checkPermission("sys:user:delete"));
-            SaRouter.match(SaHttpMethod.PUT).match(apiVersion + "/sys/user", r -> StpUtil.checkPermission("sys:user:update"));
-            SaRouter.match(SaHttpMethod.GET).match(apiVersion + "/sys/user", r -> StpUtil.checkPermission("sys:user:query"));
-            SaRouter.match(SaHttpMethod.PUT).match(apiVersion + "/sys/user/resetPassword", r -> StpUtil.checkPermission("sys:user:update"));
+            SaRouter.match(SaHttpMethod.POST).match("/sys/user", r -> StpUtil.checkPermission("sys:user:add"));
+            SaRouter.match(SaHttpMethod.DELETE).match("/sys/user", r -> StpUtil.checkPermission("sys:user:delete"));
+            SaRouter.match(SaHttpMethod.PUT).match("/sys/user", r -> StpUtil.checkPermission("sys:user:update"));
+            SaRouter.match(SaHttpMethod.GET).match("/sys/user", r -> StpUtil.checkPermission("sys:user:query"));
+            SaRouter.match(SaHttpMethod.PUT).match("/sys/user/resetPassword", r -> StpUtil.checkPermission("sys:user:update"));
             // 角色管理
-            SaRouter.match(SaHttpMethod.POST).match(apiVersion + "/sys/role", r -> StpUtil.checkPermission("sys:role:add"));
-            SaRouter.match(SaHttpMethod.DELETE).match(apiVersion + "/sys/role", r -> StpUtil.checkPermission("sys:role:delete"));
-            SaRouter.match(SaHttpMethod.PUT).match(apiVersion + "/sys/role", r -> StpUtil.checkPermission("sys:role:update"));
-            SaRouter.match(SaHttpMethod.GET).match(apiVersion + "/sys/role", r -> StpUtil.checkPermission("sys:role:query"));
-            SaRouter.match(SaHttpMethod.PUT).match(apiVersion + "/sys/role/assignPermission", r -> StpUtil.checkPermission("sys:role:assignPermission"));
+            SaRouter.match(SaHttpMethod.POST).match("/sys/role", r -> StpUtil.checkPermission("sys:role:add"));
+            SaRouter.match(SaHttpMethod.DELETE).match("/sys/role", r -> StpUtil.checkPermission("sys:role:delete"));
+            SaRouter.match(SaHttpMethod.PUT).match("/sys/role", r -> StpUtil.checkPermission("sys:role:update"));
+            SaRouter.match(SaHttpMethod.GET).match("/sys/role", r -> StpUtil.checkPermission("sys:role:query"));
+            SaRouter.match(SaHttpMethod.PUT).match("/sys/role/assignPermission", r -> StpUtil.checkPermission("sys:role:assignPermission"));
             // 权限管理
-            SaRouter.match(SaHttpMethod.POST).match(apiVersion + "/sys/permission", r -> StpUtil.checkPermission("sys:permission:add"));
-            SaRouter.match(SaHttpMethod.DELETE).match(apiVersion + "/sys/permission", r -> StpUtil.checkPermission("sys:permission:delete"));
-            SaRouter.match(SaHttpMethod.PUT).match(apiVersion + "/sys/permission", r -> StpUtil.checkPermission("sys:permission:update"));
-            SaRouter.match(SaHttpMethod.GET).match(apiVersion + "/sys/permission", r -> StpUtil.checkPermission("sys:permission:query"));
-            SaRouter.match(SaHttpMethod.GET).match(apiVersion + "/sys/permission/menu/tree", r -> StpUtil.checkPermission("sys:permission:query"));
+            SaRouter.match(SaHttpMethod.POST).match("/sys/permission", r -> StpUtil.checkPermission("sys:permission:add"));
+            SaRouter.match(SaHttpMethod.DELETE).match("/sys/permission", r -> StpUtil.checkPermission("sys:permission:delete"));
+            SaRouter.match(SaHttpMethod.PUT).match("/sys/permission", r -> StpUtil.checkPermission("sys:permission:update"));
+            SaRouter.match(SaHttpMethod.GET).match("/sys/permission", r -> StpUtil.checkPermission("sys:permission:query"));
+            SaRouter.match(SaHttpMethod.GET).match("/sys/permission/menu/tree", r -> StpUtil.checkPermission("sys:permission:query"));
             // 系统公告管理
-            SaRouter.match(SaHttpMethod.POST).match(apiVersion + "/sys/notice", r -> StpUtil.checkPermission("sys:notice:add"));
-            SaRouter.match(SaHttpMethod.DELETE).match(apiVersion + "/sys/notice", r -> StpUtil.checkPermission("sys:notice:delete"));
-            SaRouter.match(SaHttpMethod.PUT).match(apiVersion + "/sys/notice", r -> StpUtil.checkPermission("sys:notice:update"));
-            SaRouter.match(SaHttpMethod.GET).match(apiVersion + "/sys/notice", r -> StpUtil.checkPermission("sys:notice:query"));
+            SaRouter.match(SaHttpMethod.POST).match("/sys/notice", r -> StpUtil.checkPermission("sys:notice:add"));
+            SaRouter.match(SaHttpMethod.DELETE).match("/sys/notice", r -> StpUtil.checkPermission("sys:notice:delete"));
+            SaRouter.match(SaHttpMethod.PUT).match("/sys/notice", r -> StpUtil.checkPermission("sys:notice:update"));
+            SaRouter.match(SaHttpMethod.GET).match("/sys/notice", r -> StpUtil.checkPermission("sys:notice:query"));
         }));
     }
 }
