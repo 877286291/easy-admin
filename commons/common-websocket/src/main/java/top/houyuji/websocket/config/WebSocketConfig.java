c36af9794ef398c2905aad61456744f61886d8ca
houyj-dn-sy <yuji.hou@unitalen.com>
2025-02-18 13:17:06 +0800

    fix: 修改websocket

--- commons/common-websocket/src/main/java/top/houyuji/websocket/config/WebSocketConfig.java
+++ commons/common-websocket/src/main/java/top/houyuji/websocket/config/WebSocketConfig.java
@@ -1,19 +1,15 @@
 package top.houyuji.websocket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import top.houyuji.websocket.handler.MyWebSocketHandler;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // 注册的端点
-        registry.addHandler(new MyWebSocketHandler(), "/ws")
-                .setAllowedOrigins("*");
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
